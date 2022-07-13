5adaa2d928edf4a89280d7d0c8ae6decd633b7f6
Jonathan Cheng <jcheng461@gmail.com>
2022-07-13 15:54:41 -0700

    update app name in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: "Name of App Here",
+      title: "Dino Ipsum API",
       template: "./src/index.html",
       inject: "body",
     }),
